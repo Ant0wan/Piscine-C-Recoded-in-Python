a2805d0a5dfb8e8c57d2d44413d05691ce9f60af
abarthe <antoinepaulbarthelemy@gmail.com>
2018-09-13 17:01:12 +0200

    ft next prime ok with reccursion

---
+++ ft_ﬁnd_next_prime.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+def ft_is_prime(nb):
+    i = 2
+    count = 0
+    if nb == 0 or nb == 1:
+        return 0
+    elif nb >= 2:
+        while nb % i != 0 and i < nb:
+            i += 1
+        if i >= nb:
+            return 1
+        return 0
+
+def ft_ﬁnd_next_prime(nb):
+    if ft_is_prime(nb):
+        return nb
+    else:
+        return ft_ﬁnd_next_prime(nb + 1)
