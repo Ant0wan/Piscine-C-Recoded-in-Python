b451886fd786b00ca271dc3a0afac3e516c4e1f2
abarthe <antoinepaulbarthelemy@gmail.com>
2018-09-13 14:25:13 +0200

    strrev new way and atoi strat

---
+++ ft_atoi.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+def ft_atoi(str):
+    
+    return  alpha
--- ft_strrev.py
+++ ft_strrev.py
@@ -1,13 +1,4 @@
 #!/usr/bin/env python
 
 def ft_strrev(str):
-    a = 0
-    i = 0
-    while str[i:]:
-        i += 1
-    tmp_str = [''] * i
-    i -= 1
-    while a <= i:
-        tmp_str[a] = str[i - a]
-        a += 1
-    return ''.join(tmp_str)
+    return str[::-1]
