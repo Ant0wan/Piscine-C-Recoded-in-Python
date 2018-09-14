f687e47c2c01d9c85fa69f83a4ba03daa7388354
abarthe <antoinepaulbarthelemy@gmail.com>
2018-09-14 17:53:42 +0200

    more ft day 05

---
+++ ft_str_is_lowercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+def ft_str_is_lowercase(str):
+    if str == '':
+        return 1
+    else:
+        i = 0
+        while str[i:]:
+            if str[i] >= 'a' and str[i] <= 'z':
+                i += 1
+            else:
+                return 0
+        return 1
