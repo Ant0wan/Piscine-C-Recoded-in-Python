8f7ae117dce5b76436a6560c44c4657caea39ba7
abarthe <antoinepaulbarthelemy@gmail.com>
2018-09-14 17:35:54 +0200

    some more on formatting day 05

---
+++ ft_str_is_alpha.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+def ft_str_is_alpha(str):
+    i = 0
+    while str[i:]:
+        if str[i] < 'A' or (str[i] > 'Z' and str[i] < 'a') or str[i] > 'z':
+            return 0
+        i += 1
+    return 1
---
+++ ft_str_is_numeric.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+def ft_str_is_numeric(str):
+    try:
+        if int(str):
+            return 1
+    except (TypeError, ValueError, AttributeError):
+        return 0
+    i = 0
+    while str[i:]:
+        try:
+            if int(str[i]) or int(str[i]) == 0:
+                i += 1
+        except (TypeError, ValueError, AttributeError):
+            return 0
+    return 1
