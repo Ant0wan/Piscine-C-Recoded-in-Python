a9bf06ec3655fc1bc70c2e19330182d49de3e73c
abarthe <antoinepaulbarthelemy@gmail.com>
2018-09-14 18:08:04 +0200

    more on ft day 05

---
+++ ft_str_is_printable.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+def ft_str_is_printable(str):
+    if str == '':
+        return 1
+    else:
+        i = 0
+        while str[i:]:
+            if ord(str[i]) > 0 and ord(str[i]) < 32:
+                return 0
+            else:
+                i += 1
+        return 1
