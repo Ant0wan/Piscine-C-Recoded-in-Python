88b002a8f6fa374138500098cd37f30182da7a5d
abarthe <antoinepaulbarthelemy@gmail.com>
2018-09-15 14:49:42 +0200

    second shot on the go ft putnbr base

--- ft_putnbr_base.py
+++ ft_putnbr_base.py
@@ -7,6 +7,7 @@ def base_check(base):
         while base[i:]:
             assert base[i] != '+'
             assert base[i] != '-'
+            assert base[i] not in base[i + 1:]
             i += 1
         assert i > 1
     except (AssertionError, ValueError, TypeError, AttributeError):
@@ -16,6 +17,12 @@ def base_check(base):
 
 def ft_putnbr_base(nbr, base):
     if base_check(base):
-        #code here
+        strnbr = str(nbr)
+        nbr_base = []
+        i = 0
+        while strnbr[i:]:
+            nbr_base.append(base[int(strnbr[i])])
+            i += 1
+        return ''join(nbr_base)
     else:
         return 0
