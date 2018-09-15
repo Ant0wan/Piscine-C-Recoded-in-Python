dafef87e6b5af193b55ea184fc76e41cebe6b96d
abarthe <antoinepaulbarthelemy@gmail.com>
2018-09-15 23:24:25 +0200

    all bug on atois rect

--- ft_atoi.py
+++ ft_atoi.py
@@ -4,17 +4,15 @@ def ft_atoi(str):
     i = 0
     sign = 1
     number = []
-    while str[i] == ' ' or str[i] == '\t' or str[i] == '\n' or str[i] == '\v' or str[i] == '\f':
+    while str[i:] and (str[i] == ' ' or str[i] == '\t' or str[i] == '\n' or str[i] == '\v' or str[i] == '\f'):
         i += 1
-    if str[i] == '-' or str[i] == '+':
+    if str[i:] and (str[i] == '-' or str[i] == '+'):
         sign = -1 if str[i] == '-' else 1
         i += 1
-    while str[i].isdigit():
+    while str[i:] and str[i].isdigit():
         number.append(str[i])
         i += 1
     try:
         return int(''.join(number)) * sign
     except (TypeError, ValueError, AttributeError):
-        return
-
-print(ft_atoi('     -42dzdzd'))
+        return None
--- ft_atoi_base.py
+++ ft_atoi_base.py
@@ -16,21 +16,24 @@ def base_check(base):
         return 1
 
 def ft_atoi_base(str, base):
-    if base_check(base) and str != '':
-        i = 0
+    i = 0
+    if base_check(base) and str[i:]:
         sign = 1
-        number = 0
-        while str[i] == ' ' or str[i] == '\t' or str[i] == '\n' or str[i] == '\v' or str[i] == '\f':
+        while str[i:] and (str[i] == ' ' or str[i] == '\t' or str[i] == '\n' or str[i] == '\v' or str[i] == '\f'):
             i += 1
-        if str[i] == '-' or str[i] == '+':
+        if str[i:] and (str[i] == '-' or str[i] == '+'):
             sign = -1 if str[i] == '-' else 1
             i += 1
         length_base = 0
         while base[length_base:]:
             length_base += 1
+        if str[i:]:
+            number = 0
+        else:
+            return None
         while str[i:] and str[i] in base:
             number = number * length_base + base.index(str[i])
             i += 1
         return number * sign
-    else :
-        return 0
+    else:
+        return None
