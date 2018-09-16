b4680af33b7b411b1ea6d2c49d0f2df5166fcc28
abarthe <antoinepaulbarthelemy@gmail.com>
2018-09-16 16:31:19 +0200

    ft_putstr_non_printable first shot

--- ft_putstr_non_printable.py
+++ ft_putstr_non_printable.py
@@ -1,4 +1,18 @@
+#!/usr/bin/env python
+
 NON_PRINTABLE = lambda x: True if (ord(x) >= 0 and ord(x) < 32) else False
+BASE = '0123456789abcdef'
+
+def ft_putnbr_base(nbr, base):
+    nbr_base = []
+    rest = nbr
+    i = 0
+    while base[i:]:
+        i += 1
+    while rest != 0:
+        nbr_base.insert(0, base[rest % i])
+        rest = rest // i
+    return ''.join(nbr_base)
 
 def ft_putstr_non_printable(str):
     if str != '':
@@ -6,12 +20,12 @@ def ft_putstr_non_printable(str):
         i = 0
         while str[i:]:
             if NON_PRINTABLE(str[i]):
-                cp_str.append('N')
+                add = ft_putnbr_base(ord(str[i]), BASE)
+                special = '0' if ord(str[i]) <= 15 else ''
+                cp_str.append('\\' + special + add)
             else:
                 cp_str.append(str[i])
             i += 1
         return ''.join(cp_str)
     else:
         return None
-
-print(ft_putstr_non_printable('Hello'))
