4c50d338185f027f687c47e6e3fdf831326fb569
abarthe <antoinepaulbarthelemy@gmail.com>
2018-09-16 23:53:54 +0200

    ft print memory HS, needs C module to get address

--- ft_print_memory.py
+++ ft_print_memory.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 
+  # NOT COMPLETE
+  # The follwing needs a C modules to get the value stored in
+  # the giving address, to be done late..
+
+# Imports the C extensions needed to use call by reference
+from ctypes import *
+# Defines global variable
 BASE = '0123456789abcdef'
 
 def ft_putnbr_base(nbr, base):
@@ -22,10 +29,17 @@ def ft_print_memory(addr, size):
     return print(addr)
 # Need to code with C some extensions...
 
-
-
+# MAIN
 ligne = 'Salut les aminches c\'est cool show mem on fait de truc terrible.'
 print(len(ligne))
 print(id(ligne), id(ligne[0]), id(ligne[1]))
 print(ligne[0], ft_putnbr_base(ord(ligne[1]), BASE))
 #ft_print_memory(120, 10)
+i = c_int(42)
+pi = pointer(i)
+print(str(pi)[33:40])
+ad_pi = addressof(pi)
+print(hex(ad_pi).upper())
+print (pi)
+toto = pi
+print(pi.contents)
