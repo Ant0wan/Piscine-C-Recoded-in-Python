e89da2280896aad8e0f3bedf45bacfeeec3ba558
abarthe <antoinepaulbarthelemy@gmail.com>
2018-09-17 16:23:00 +0000

    changed for exe

--- ft_print_program_name.py
+++ ft_print_program_name.py
@@ -1,12 +1,12 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 
 def ft_print_program_name():
-    print(sys.argv[0], end='')
+    print(sys.argv[0], end='\n')
 
 def main():
     ft_print_program_name()
 
-if __main__ == '__main__':
+if __name__ == '__main__':
     main()
