0c7639b55ab1c05d750aa4ec36916a0a3e3e8f42
abarthe <antoinepaulbarthelemy@gmail.com>
2018-09-18 16:03:54 +0200

    done with class obj

--- ft_param_to_tab.py
+++ ft_param_to_tab.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import sys
 
 class s_stock_par :
@@ -11,12 +9,6 @@ def ft_param_to_tab():
     tab_param = []
     while sys.argv[i:]:
         tab_param.append(s_stock_par(sys.argv[i]))
-
         i += 1
+    tab_param.append(0)
     return tab_param
-
-def main():
-    print(ft_param_to_tab())
-
-if __name__ == "__main__":
-    main()
