40130a40dc7b384e639e569a460e2c0148f9a497
abarthe <antoinepaulbarthelemy@gmail.com>
2018-09-18 16:18:18 +0200

    class modified

--- ft_param_to_tab.py
+++ ft_param_to_tab.py
@@ -1,8 +1,29 @@
 import sys
 
+def ft_strlen(str):
+    i = 0
+    while str[i:]:
+        i += 1
+    return i
+
+def ft_split_whitespaces(str):
+    i = 0
+    table = []
+    while str[i:]:
+        word = []
+        while str[i:] and (str[i] != ' ' and str[i] != '\t' and str[i] != '\n'):
+            word.append(str[i])
+            i += 1
+        table.append(''.join(word))
+        while str[i:] and (str[i] == ' ' or str[i] == '\t' or str[i] == '\n'):
+            i += 1
+    return table
+
 class s_stock_par :
     def __init__(self, argv):
-        self.argv = argv
+        self.size_param = ft_strlen(argv)
+        self.argv = id(argv)
+        self.copy = ft_split_whitespaces(argv)
 
 def ft_param_to_tab():
     i = 0
